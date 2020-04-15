163479e6b9e2cd60c03cb10d1e8700b08dd75b45
Kenji Saito <ken-yo@mbr.nifty.com>
2020-04-15 22:59:37 +0900

    自動アップデート workflow を追加 (#7)

---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+CUR=$(pwd)
+
+YARN_PROJECTS="\
+  amplify-js-app\
+  dockerode-example\
+  next-examples/blog/next-amplify-example\
+  next-examples/next-amplify-example\
+  next-examples/next-auth0-example\
+  next-examples/next-ts-examples\
+  nuxt-example/hello-world\
+  nuxt-example/nuxt-ts-spa-example\
+  nuxt-example/nuxt-typescript-example\
+  nuxt-example/nuxt-vuetify-example
+  react-example/auth0-example
+"
+
+for target in ${YARN_PROJECTS}
+do
+  cd ${CUR}/${target}
+  yarn upgrade
+  yarn install
+done
+
+
+NPM_PROJECTS="elm-electron hello-electron"
+
+for target in ${NPM_PROJECTS}
+do
+  cd ${CUR}/${target}
+  npm update
+  npm upgrade
+done
+
+cd ${CUR}
