402a585cd64e47aa744da7d979ef748ff894d826
Kenji Saito <ken-yo@mbr.nifty.com>
2020-11-03 00:56:09 +0900

    見てくれをそれらしく変更

--- next-examples/cognito-s3-example/src/components/s3directory/index.tsx
+++ next-examples/cognito-s3-example/src/components/s3directory/index.tsx
@@ -1,25 +1,41 @@
-import { useEffect, useState } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import FolderIcon from '@material-ui/icons/Folder';
+
+import Divider from '@material-ui/core/Divider';
 
 export interface S3DirectoryProp {
     s3keys: string[]
 }
 
-
 const S3Directory = (props: S3DirectoryProp): JSX.Element => {
-    const [ entries, setEntries ] = useState<string[]>([]);
-
-    useEffect(() => {
-        setEntries(props.s3keys
-            .filter(key => key.endsWith('index.html')));
-
-    }, [entries]);
 
     return (
-        <ul>
-        {
-            entries.map((entry: string): JSX.Element => (<li><a href={`${entry}`}>{entry}</a></li>))
-        }
-        </ul>
+        <div>
+            <List>
+                {
+                    props.s3keys
+                        .filter(key => key.endsWith('index.html'))
+                        .map((entry: string): JSX.Element => (
+                            <a href={`${entry}`} key={entry}>
+                                <Divider />
+                                <ListItem>
+                                    <ListItemAvatar>
+                                        <Avatar>
+                                            <FolderIcon />
+                                        </Avatar>
+                                    </ListItemAvatar>
+                                    <ListItemText primary={entry} />
+                                </ListItem>
+                            </a>
+                        ))
+                }
+            </List>
+            <Divider />
+        </div>
     );
 }
 
--- next-examples/cognito-s3-example/src/pages/index.tsx
+++ next-examples/cognito-s3-example/src/pages/index.tsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
-import Amplify, { Auth, Hub } from 'aws-amplify';
+import Amplify from 'aws-amplify';
+
 import { AmplifyAuthenticator, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react';
-import { CognitoUser } from 'amazon-cognito-identity-js';
+import { AuthState, onAuthUIStateChange, CognitoUserInterface } from '@aws-amplify/ui-components';
 import Storage from '@aws-amplify/storage';
+
+import Layout from 'components/Layout';
 import styles from '../styles/Home.module.css';
 import awsconfig from '../aws-config';
+
 import S3Directory from 'components/s3directory';
 
 Amplify.configure(awsconfig);
@@ -13,7 +16,9 @@ Amplify.configure(awsconfig);
 
 Storage.configure({
   customPrefix: {
-      public: ''
+    public: '',
+    protected: '',
+    private: ''
   }
 })
 
@@ -27,65 +32,33 @@ interface ListObjectItem {
 type ListObjectResponse = ListObjectItem[];
 
 const Home = (): JSX.Element => {
-  const [user, setUser] = useState<CognitoUser | undefined>(undefined);
-  const [list, setList] = useState<ListObjectItem[]>([]);
+  const [authState, setAuthState] = useState<AuthState>();
+  const [user, setUser] = useState<CognitoUserInterface | undefined>(undefined);
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData as CognitoUserInterface);
+    });
+  }, []);
 
-  const listFiles = () => {
+  const [list, setList] = useState<ListObjectItem[]>([]);
+  useEffect(() => {
+    // if (authState === AuthState.SignedIn && user) {
     Promise.all([Storage.list('contents')
       .then((result: ListObjectResponse) => {
+        // console.log(result);
         setList(result);
       })
-    // eslint-disable-next-line no-console
+      // eslint-disable-next-line no-console
       .catch((err) => console.log(err))]);
-  };
+    // }
+  }, []);
 
-  const getUser = async (): Promise<void> => {
-    Auth.currentAuthenticatedUser()
-      .then((result) => {
-        if (result !== undefined && result.signInUserSession !== undefined) {
-          setUser(result as CognitoUser);
-        } else {
-          setUser(undefined);
-        }
-      })
-      .catch((e) => {
-        // eslint-disable-next-line no-console
-        console.error(e);
-        setUser(undefined);
-      });
-  };
-
-  Hub.listen('auth', async (data) => {
-    switch (data.payload.event) {
-      case 'signIn':
-        await getUser();
-        break;
-      case 'signOut':
-        setUser(undefined);
-        break;
-      default:
-        break;
-    }
-  });
-
-  useEffect(() => {
-    if (user !== undefined) {
-      listFiles();
-    } else {
-      getUser();
-      if (user !== undefined) {
-        listFiles();
-      }
-    }
-  }, [user, list]);
+  console.log(authState);
+  console.log(user?.username);
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>メインページ</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+    <Layout username={user?.username}>
       <AmplifyAuthenticator>
         <AmplifySignIn slot="sign-in">
           <div slot="federated-buttons"></div>
@@ -93,26 +66,31 @@ const Home = (): JSX.Element => {
         </AmplifySignIn>
 
         <main className={styles.main}>
+
           <div>
             <AmplifySignOut />
           </div>
-          <ul>
-            <S3Directory s3keys={list.map(item => item.key)} />
-          </ul>
-        </main>
+          {
+            authState === AuthState.SignedIn ? (
 
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-          </a>
-        </footer>
+                <S3Directory s3keys={list.map(item => item.key)} />
+
+            ) : ""
+          }
+          </main>
       </AmplifyAuthenticator>
-    </div>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </Layout>
   );
 };
 
