6d31ceaef6f81f39b3a04ab0f3d5c81c8023fe35
Kenji Saito <ken-yo@mbr.nifty.com>
2021-01-16 14:39:36 +0900

    Remove the react project

---
+++ react-example/auth0-example/build/precache-manifest.debff18aa0eed6df6af82e8f71e3ce5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84b2ef8a6a60a18b16d97a69a4994cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "098bea7f2a9fec935a1a",
+    "url": "/static/js/2.2f46a54d.chunk.js"
+  },
+  {
+    "revision": "0adf7f1b9434a120a5bce5b5824fa305",
+    "url": "/static/js/2.2f46a54d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7052ee3293d1dba5e6",
+    "url": "/static/js/main.74283d21.chunk.js"
+  },
+  {
+    "revision": "266d42eaf45d9d7200dc",
+    "url": "/static/js/runtime-main.0f513a74.js"
+  }
+]);
