5bb8148471f5c5925352db11cd085807dc8ccf18
Kenji Saito <ken-yo@mbr.nifty.com>
2021-06-08 21:35:52 +0900

    Add drawer menu (#250)

--- next-examples/vnc-example/src/component/VncClient/index.tsx
+++ next-examples/vnc-example/src/component/VncClient/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { VncScreen } from 'react-vnc';
 
 interface VncClientProps {
@@ -7,18 +7,27 @@ interface VncClientProps {
 }
 
 const VncClient: React.FunctionComponent<VncClientProps> = (props): JSX.Element => {
-  const [url] = useState(props.url);
+  const [url, setUrl] = useState('');
+
+  useEffect(
+    () => {
+      setUrl(props.url);
+    }, [props],
+  );
 
   return url.length > 0 ? (
-    <VncScreen
-      url={url}
-      scaleViewport
-      background="#000000"
-      style={{
-        width: '75vw',
-        height: '75vh',
-      }}
-    />
+    <>
+      <VncScreen
+        url={url}
+        scaleViewport
+        background="#000000"
+        resizeSession={true}
+        style={{
+          width: '95vw',
+          height: '95vh',
+        }}
+      />
+    </>
   ) : (<div>VNC URL not provided.</div>);
 };
 
